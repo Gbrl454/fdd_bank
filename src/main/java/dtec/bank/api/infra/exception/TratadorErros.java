915d300f845291600c44348f976e755776e657a4
951549700@adm.unifor.br <gabrielmatos@unifor.br>
2023-07-06 12:08:26 -0300

    Organizando mensagens de erro

---
+++ src/main/java/dtec/bank/api/infra/exception/TratadorErros.java
@@ -0,0 +1,29 @@
+package dtec.bank.api.infra.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorErros {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404 (EntityNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400 (MethodArgumentNotValidException e) {
+        var erros = e.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao (FieldError er) {
+            this(er.getField(), er.getDefaultMessage());
+        }
+    }
+}
