c4425db53a0dc830738d4deea59b2c9de29ea195
951549700 <gabrielmatos@unifor.br>
2023-08-01 16:08:08 -0300

    Colocar indicador de pais no Banco

---
+++ src/main/java/dtec/bank/api/utils/Pais.java
@@ -0,0 +1,28 @@
+package dtec.bank.api.utils;
+
+public enum Pais {
+    BRA ("Brasil", "PT-BR", 3),
+    USA("Estados Unidos da America", "EN-US",-2);
+
+    private String nome;
+    private String idioma;
+    private int utc;
+
+    Pais (String nome, String idioma, int utc) {
+        this.nome = nome;
+        this.idioma = idioma;
+        this.utc = utc;
+    }
+
+    public String getNome () {
+        return nome;
+    }
+
+    public String getIdioma () {
+        return idioma;
+    }
+
+    public int getUtc () {
+        return utc;
+    }
+}
