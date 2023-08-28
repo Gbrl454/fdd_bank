1569a6b368b68058d43e5e214bee8b035c5e9d22
951549700 <gabrielmatos@unifor.br>
2023-08-28 17:16:44 -0300

    Tem que arrumar isso aqui

--- backend/src/main/java/dtec/bank/api/config/SecurityConfigurations.java
+++ backend/src/main/java/dtec/bank/api/config/SecurityConfigurations.java
@@ -16,12 +16,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
     @Autowired
     private SecurityFilter securityFilter;
 
+    // TODO tá td quebrado, viver não faz sentido
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable).sessionManagement(
