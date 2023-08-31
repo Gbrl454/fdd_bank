c043d8827196f40e889abd25fe0fa5d97b8c305e
951549700 <gabrielmatos@unifor.br>
2023-08-31 14:32:40 -0300

    Tela de Transferência

--- frontend/src/app/pages/transferencia/transferencia.component.ts
+++ frontend/src/app/pages/transferencia/transferencia.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-transferencia',
   templateUrl: './transferencia.component.html',
-  styleUrls: ['./transferencia.component.scss']
+  styleUrls: ['./transferencia.component.scss'],
 })
 export class TransferenciaComponent {
+  formTransf!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formTransf = this.formBuilder.group({
+      valor: ['', Validators.compose([Validators.required])],
+      idDestino: ['', Validators.compose([Validators.required])],
+    });
+  }
+
+  habilitarBotao(): string {
+    return this.formTransf.valid ? 'botao' : 'botao_desabilitado';
+  }
+
+  async onSubmit() {}
 }
